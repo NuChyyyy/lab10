9a0316d659c4ded321f659ec93aa41eaf9bafe96
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2021-01-12 15:28:24 +0700

    lab10-1*edit
    Signed-off-by: NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,34 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i = 0; i < 3; i++)
+    {
+        cin.get();
+    }
+    int grade;
+    srand(time(0));
+    grade = rand()%9 + 1;
+    string g;
+
+    switch(grade)
+    {
+        case 1 : g = "A" ; break;
+        case 2 : g = "B+"; break;
+        case 3 : g = "B"; break;
+        case 4 : g = "C+" ; break;
+        case 5 : g = "C" ; break;
+        case 6 : g = "D+" ; break;
+        case 7 : g = "D" ; break;
+        case 8 : g = "F" ; break;
+        case 9 : g = "W" ; break;
+    }
+    cout << "You will get "<< g <<" in this 261102.";
+    return 0;
+}
